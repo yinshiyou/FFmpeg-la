1f7eda938ae180650e626d1583f4625c5951758a
chen52 <chen52@loongson-pc>
2021-06-19 15:31:56 +0800

    avcodec/la: Fix undefined reference to VP9 LSX optimizations.
    Fix undefined reference error when building with -O0 and -disable-lsx.
    
    Change-Id: I773230ec270b3a6bcd79a711bd1fc33f3399acf5
    Signed-off-by: chen52 <chen52@loongson-pc>

--- libavcodec/loongarch/vp9dsp_init_loongarch.c
+++ libavcodec/loongarch/vp9dsp_init_loongarch.c
@@ -24,9 +24,6 @@
 #include "libavcodec/vp9dsp.h"
 #include "vp9dsp_loongarch.h"
 
-static av_cold void vp9dsp_mc_init_lsx(VP9DSPContext *dsp, int bpp)
-{
-    if (bpp == 8) {
 #define init_subpel1(idx1, idx2, idxh, idxv, sz, dir, type)  \
     dsp->mc[idx1][FILTER_8TAP_SMOOTH ][idx2][idxh][idxv] =   \
         ff_##type##_8tap_smooth_##sz##dir##_lsx;             \
@@ -47,36 +44,37 @@ static av_cold void vp9dsp_mc_init_lsx(VP9DSPContext *dsp, int bpp)
     init_subpel2(idx, 0, 1, v, type);   \
     init_subpel2(idx, 1, 1, hv, type);
 
-    init_subpel3(0, put);
-    init_subpel3(1, avg);
-
-#undef init_subpel1
-#undef init_subpel2
-#undef init_subpel3
-
-#define init_fpel(idx1, idx2, sz, type)                                \
-dsp->mc[idx1][FILTER_8TAP_SMOOTH ][idx2][0][0] = ff_##type##sz##_lsx;  \
-dsp->mc[idx1][FILTER_8TAP_REGULAR][idx2][0][0] = ff_##type##sz##_lsx;  \
-dsp->mc[idx1][FILTER_8TAP_SHARP  ][idx2][0][0] = ff_##type##sz##_lsx;  \
-dsp->mc[idx1][FILTER_BILINEAR    ][idx2][0][0] = ff_##type##sz##_lsx
+#define init_fpel(idx1, idx2, sz, type)                                    \
+    dsp->mc[idx1][FILTER_8TAP_SMOOTH ][idx2][0][0] = ff_##type##sz##_lsx;  \
+    dsp->mc[idx1][FILTER_8TAP_REGULAR][idx2][0][0] = ff_##type##sz##_lsx;  \
+    dsp->mc[idx1][FILTER_8TAP_SHARP  ][idx2][0][0] = ff_##type##sz##_lsx;  \
+    dsp->mc[idx1][FILTER_BILINEAR    ][idx2][0][0] = ff_##type##sz##_lsx
 
 #define init_copy(idx, sz)                    \
     init_fpel(idx, 0, sz, copy);              \
     init_fpel(idx, 1, sz, avg)
 
-    init_copy(0, 64);
-    init_copy(1, 32);
-    init_copy(2, 16);
-    init_copy(3, 8);
-
-#undef init_copy
-#undef init_fpel
+static av_cold void vp9dsp_mc_init_lsx(VP9DSPContext *dsp, int bpp)
+{
+    int cpu_flags = av_get_cpu_flags();
+    if (have_lsx(cpu_flags)) {
+        if (bpp == 8) {
+            init_subpel3(0, put);
+            init_subpel3(1, avg);
+            init_copy(0, 64);
+            init_copy(1, 32);
+            init_copy(2, 16);
+            init_copy(3, 8);
+        }
     }
 }
 
-static av_cold void vp9dsp_intrapred_init_lsx(VP9DSPContext *dsp, int bpp)
-{
-    if (bpp == 8) {
+#undef init_subpel1
+#undef init_subpel2
+#undef init_subpel3
+#undef init_copy
+#undef init_fpel
+
 #define init_intra_pred_lsx(tx, sz)                             \
     dsp->intra_pred[tx][VERT_PRED]    = ff_vert_##sz##_lsx;     \
     dsp->intra_pred[tx][HOR_PRED]     = ff_hor_##sz##_lsx;      \
@@ -88,68 +86,82 @@ static av_cold void vp9dsp_intrapred_init_lsx(VP9DSPContext *dsp, int bpp)
     dsp->intra_pred[tx][DC_129_PRED]  = ff_dc_129_##sz##_lsx;   \
     dsp->intra_pred[tx][TM_VP8_PRED]  = ff_tm_##sz##_lsx;       \
 
-        init_intra_pred_lsx(TX_16X16, 16x16);
-        init_intra_pred_lsx(TX_32X32, 32x32);
-#undef init_intra_pred_lsx
-
 #define init_intra_pred_lsx(tx, sz)                             \
     dsp->intra_pred[tx][DC_PRED]      = ff_dc_##sz##_lsx;       \
     dsp->intra_pred[tx][LEFT_DC_PRED] = ff_dc_left_##sz##_lsx;  \
     dsp->intra_pred[tx][TOP_DC_PRED]  = ff_dc_top_##sz##_lsx;   \
     dsp->intra_pred[tx][TM_VP8_PRED]  = ff_tm_##sz##_lsx;       \
 
-    init_intra_pred_lsx(TX_4X4, 4x4);
-    init_intra_pred_lsx(TX_8X8, 8x8);
-#undef init_intra_pred_lsx
-
+static av_cold void vp9dsp_intrapred_init_lsx(VP9DSPContext *dsp, int bpp)
+{
+    int cpu_flags = av_get_cpu_flags();
+    if (have_lsx(cpu_flags)) {
+        if (bpp == 8) {
+            init_intra_pred_lsx(TX_16X16, 16x16);
+            init_intra_pred_lsx(TX_32X32, 32x32);
+            init_intra_pred_lsx(TX_4X4, 4x4);
+            init_intra_pred_lsx(TX_8X8, 8x8);
+        }
     }
 }
 
-static av_cold void vp9dsp_itxfm_init_lsx(VP9DSPContext *dsp, int bpp)
-{
-    if (bpp == 8) {
+#undef init_intra_pred_lsx
+#undef init_intra_pred_lsx
+
 #define init_idct(tx, nm)                        \
     dsp->itxfm_add[tx][DCT_DCT]   =              \
     dsp->itxfm_add[tx][ADST_DCT]  =              \
     dsp->itxfm_add[tx][DCT_ADST]  =              \
     dsp->itxfm_add[tx][ADST_ADST] = nm##_add_lsx
 
-    init_idct(TX_32X32, ff_idct_idct_32x32);
-#undef init_idct
+static av_cold void vp9dsp_itxfm_init_lsx(VP9DSPContext *dsp, int bpp)
+{
+    int cpu_flags = av_get_cpu_flags();
+    if (have_lsx(cpu_flags)) {
+        if (bpp == 8) {
+            init_idct(TX_32X32, ff_idct_idct_32x32);
+        }
     }
 }
 
+#undef init_idct
 
 static av_cold void vp9dsp_loopfilter_init_lsx(VP9DSPContext *dsp, int bpp)
 {
-    if (bpp == 8) {
-        dsp->loop_filter_8[0][0] = ff_loop_filter_h_4_8_lsx;
-        dsp->loop_filter_8[0][1] = ff_loop_filter_v_4_8_lsx;
-        dsp->loop_filter_8[1][0] = ff_loop_filter_h_8_8_lsx;
-        dsp->loop_filter_8[1][1] = ff_loop_filter_v_8_8_lsx;
-        dsp->loop_filter_8[2][0] = ff_loop_filter_h_16_8_lsx;
-        dsp->loop_filter_8[2][1] = ff_loop_filter_v_16_8_lsx;
+    int cpu_flags = av_get_cpu_flags();
+    if (have_lsx(cpu_flags)) {
+        if (bpp == 8) {
+            dsp->loop_filter_8[0][0] = ff_loop_filter_h_4_8_lsx;
+            dsp->loop_filter_8[0][1] = ff_loop_filter_v_4_8_lsx;
+            dsp->loop_filter_8[1][0] = ff_loop_filter_h_8_8_lsx;
+            dsp->loop_filter_8[1][1] = ff_loop_filter_v_8_8_lsx;
+            dsp->loop_filter_8[2][0] = ff_loop_filter_h_16_8_lsx;
+            dsp->loop_filter_8[2][1] = ff_loop_filter_v_16_8_lsx;
 
-        dsp->loop_filter_16[0] = ff_loop_filter_h_16_16_lsx;
-        dsp->loop_filter_16[1] = ff_loop_filter_v_16_16_lsx;
+            dsp->loop_filter_16[0] = ff_loop_filter_h_16_16_lsx;
+            dsp->loop_filter_16[1] = ff_loop_filter_v_16_16_lsx;
 
-        dsp->loop_filter_mix2[0][0][0] = ff_loop_filter_h_44_16_lsx;
-        dsp->loop_filter_mix2[0][0][1] = ff_loop_filter_v_44_16_lsx;
-        dsp->loop_filter_mix2[0][1][0] = ff_loop_filter_h_48_16_lsx;
-        dsp->loop_filter_mix2[0][1][1] = ff_loop_filter_v_48_16_lsx;
-        dsp->loop_filter_mix2[1][0][0] = ff_loop_filter_h_84_16_lsx;
-        dsp->loop_filter_mix2[1][0][1] = ff_loop_filter_v_84_16_lsx;
-        dsp->loop_filter_mix2[1][1][0] = ff_loop_filter_h_88_16_lsx;
-        dsp->loop_filter_mix2[1][1][1] = ff_loop_filter_v_88_16_lsx;
+            dsp->loop_filter_mix2[0][0][0] = ff_loop_filter_h_44_16_lsx;
+            dsp->loop_filter_mix2[0][0][1] = ff_loop_filter_v_44_16_lsx;
+            dsp->loop_filter_mix2[0][1][0] = ff_loop_filter_h_48_16_lsx;
+            dsp->loop_filter_mix2[0][1][1] = ff_loop_filter_v_48_16_lsx;
+            dsp->loop_filter_mix2[1][0][0] = ff_loop_filter_h_84_16_lsx;
+            dsp->loop_filter_mix2[1][0][1] = ff_loop_filter_v_84_16_lsx;
+            dsp->loop_filter_mix2[1][1][0] = ff_loop_filter_h_88_16_lsx;
+            dsp->loop_filter_mix2[1][1][1] = ff_loop_filter_v_88_16_lsx;
+        }
     }
 }
 
 static av_cold void vp9dsp_init_lsx(VP9DSPContext *dsp, int bpp)
 {
-    vp9dsp_mc_init_lsx(dsp, bpp);
-    vp9dsp_intrapred_init_lsx(dsp, bpp);
-    vp9dsp_loopfilter_init_lsx(dsp, bpp);
-    vp9dsp_itxfm_init_lsx(dsp, bpp);
+    int cpu_flags = av_get_cpu_flags();
+    if (have_lsx(cpu_flags)) {
+        vp9dsp_mc_init_lsx(dsp, bpp);
+        vp9dsp_intrapred_init_lsx(dsp, bpp);
+        vp9dsp_loopfilter_init_lsx(dsp, bpp);
+        vp9dsp_itxfm_init_lsx(dsp, bpp);
+    }
 }
 
 av_cold void ff_vp9dsp_init_loongarch(VP9DSPContext *dsp, int bpp)
