654834a9ca73cf949d2350d8a58fcbc2385e3197
Hao Chen <chenhao@loongson.cn>
2023-06-28 17:28:38 +0800

    swscale/loongarch: Add yuv2rgb_lasx.c and rgb2rgb_lasx.c files
    1. Add yuv420_rgb24 and yuv420_bgr24 optimization functions with lasx in
    yuv2rgb_lasx.c file.
    2. Add ff_interleave_bytes_lasx optmization function with lasx in
    rgb2rgb_lasx.c file.
    
    Change-Id: Iae51705da3d4598e8e05b3218b9531415ea0e2f0
    Signed-off-by: Hao Chen <chenhao@loongson.cn>

---
+++ libswscale/loongarch/rgb2rgb_lasx.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 Loongson Technology Co. Ltd.
+ * Contributed by Hao Chen(chenhao@loongson.cn)
+ * All rights reserved.
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libswscale/rgb2rgb.h"
+#include "libavutil/loongarch/cpu.h"
+#include "libavutil/loongarch/generic_macros_lasx.h"
+
+static void ff_interleave_bytes_lasx(const uint8_t *src1, const uint8_t *src2,
+                                     uint8_t *dest, int width, int height,
+                                     int src1Stride, int src2Stride, int dstStride)
+{
+    int h;
+    int len = width & (0xFFFFFFF0);
+
+    for (h = 0; h < height; h++) {
+        int w, index = 0;
+        __m256i src_1, src_2, dst;
+
+        for (w = 0; w < len; w += 16) {
+            src_1 = LASX_LD(src1 + w);
+            src_2 = LASX_LD(src2 + w);
+            src_1 = __lasx_xvpermi_d(src_1, 0xD8);
+            src_2 = __lasx_xvpermi_d(src_2, 0xD8);
+            dst   = __lasx_xvilvl_b(src_2, src_1);
+            LASX_ST(dst, dest + index);
+            index  += 32;
+        }
+        for (w = 0; w < width; w++) {
+            dest[(w << 1) + 0] = src1[w];
+            dest[(w << 1) + 1] = src2[w];
+        }
+        dest += dstStride;
+        src1 += src1Stride;
+        src2 += src2Stride;
+    }
+}
+
+
+av_cold void rgb2rgb_init_loongarch(void)
+{
+    int cpu_flags = av_get_cpu_flags();
+#if HAVE_LASX
+    if (have_lasx(cpu_flags))
+        interleaveBytes = ff_interleave_bytes_lasx;
+#endif /* #if HAVE_LASX */
+}
---
+++ libswscale/loongarch/yuv2rgb_lasx.c
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2021 Loongson Technology Co. Ltd.
+ * Contributed by Hao Chen(chenhao@loongson.cn)
+ * All rights reserved.
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavutil/loongarch/generic_macros_lasx.h"
+#include "libswscale/rgb2rgb.h"
+#include "libswscale/swscale_internal.h"
+#include "libavutil/loongarch/cpu.h"
+
+#define YUV2RGB_LOAD_COE                                      \
+    /* Load x_offset */                                       \
+    __m256i y_offset = __lasx_xvldrepl_d(&c->yOffset, 0);     \
+    __m256i u_offset = __lasx_xvldrepl_d(&c->uOffset, 0);     \
+    __m256i v_offset = __lasx_xvldrepl_d(&c->vOffset, 0);     \
+    /* Load x_coeff  */                                       \
+    __m256i ug_coeff = __lasx_xvldrepl_d(&c->ugCoeff, 0);     \
+    __m256i vg_coeff = __lasx_xvldrepl_d(&c->vgCoeff, 0);     \
+    __m256i y_coeff  = __lasx_xvldrepl_d(&c->yCoeff,  0);     \
+    __m256i ub_coeff = __lasx_xvldrepl_d(&c->ubCoeff, 0);     \
+    __m256i vr_coeff = __lasx_xvldrepl_d(&c->vrCoeff, 0);     \
+
+#define LOAD_YUV_16                                           \
+    m_y  = LASX_LD(py + (w << 4));                            \
+    m_u  = __lasx_xvldrepl_d(pu + (w << 3), 0);               \
+    m_v  = __lasx_xvldrepl_d(pv + (w << 3), 0);               \
+    LASX_UNPCK_L_HU_BU(m_y, m_y);                             \
+    LASX_UNPCK_L_HU_BU_2(m_u, m_v, m_u, m_v);                 \
+
+/* YUV2RGB method
+ * The conversion method is as follows:
+ * R = Y' * y_coeff + V' * vr_coeff
+ * G = Y' * y_coeff + V' * vg_coeff + U' * ug_coeff
+ * B = Y' * y_coeff + U' * ub_coeff
+ *
+ * where X' = X * 8 - x_offset
+ *
+ */
+
+#define YUV2RGB                                                            \
+    m_y = __lasx_xvslli_h(m_y, 3);                                         \
+    m_u = __lasx_xvslli_h(m_u, 3);                                         \
+    m_v = __lasx_xvslli_h(m_v, 3);                                         \
+    m_y = __lasx_xvsub_h(m_y, y_offset);                                   \
+    m_u = __lasx_xvsub_h(m_u, u_offset);                                   \
+    m_v = __lasx_xvsub_h(m_v, v_offset);                                   \
+    m_u = __lasx_xvshuf_h(shuf4, m_u, m_u);                                \
+    m_v = __lasx_xvshuf_h(shuf4, m_v, m_v);                                \
+    y_1 = __lasx_xvmuh_h(m_y, y_coeff);                                    \
+    u2g = __lasx_xvmuh_h(m_u, ug_coeff);                                   \
+    r   = __lasx_xvmadd_h(y_1, m_v, vr_coeff);                             \
+    uvg = __lasx_xvmadd_h(u2g, m_v, vg_coeff);                             \
+    b   = __lasx_xvmadd_h(y_1, m_u, ub_coeff);                             \
+    g   = __lasx_xvsadd_h(y_1, uvg);                                       \
+    LASX_CLIP_H_0_255_2(r, g, r, g);                                       \
+    LASX_CLIP_H_0_255(b, b);                                               \
+
+#define RGB_PACK_16(r, g, b, rgb_l, rgb_h)                                 \
+{                                                                          \
+    __m256i rg;                                                            \
+    LASX_SHUF_B_128SV(g, r, shuf1, rg);                                    \
+    LASX_SHUF_B_2_128SV(b, rg, b, rg, shuf2, shuf3, rgb_l, rgb_h);         \
+}
+
+#define RGB_STORE(rgb_l, rgb_h, image, w)                                      \
+{                                                                              \
+    uint8_t *index = image + (w * 48);                                         \
+    __lasx_xvstelm_d(rgb_l, (index), 0,  0);                                   \
+    __lasx_xvstelm_d(rgb_l, (index), 8,  1);                                   \
+    __lasx_xvstelm_d(rgb_h, (index), 16, 0);                                   \
+    __lasx_xvstelm_d(rgb_l, (index), 24, 2);                                   \
+    __lasx_xvstelm_d(rgb_l, (index), 32, 3);                                   \
+    __lasx_xvstelm_d(rgb_h, (index), 40, 2);                                   \
+}
+
+#define YUV2RGBFUNC(func_name, dst_type, alpha)                                     \
+    static int func_name(SwsContext *c, const uint8_t *src[],                       \
+                         int srcStride[], int srcSliceY, int srcSliceH,             \
+                         uint8_t *dst[], int dstStride[])                           \
+{                                                                                   \
+    int w, y, h_size, vshift;                                                       \
+    __m256i m_y, m_u, m_v;                                                          \
+    __m256i y_1, u2g, uvg, rgb_l, rgb_h;                                            \
+    __m256i r, g, b;                                                                \
+    __m256i shuf1 = {0x1606140412021000, 0x1E0E1C0C1A0A1808,                        \
+                     0x1606140412021000, 0x1E0E1C0C1A0A1808};                       \
+    __m256i shuf2 = {0x0504120302100100, 0x0A18090816060714,                        \
+                     0x0504120302100100, 0x0A18090816060714};                       \
+    __m256i shuf3 = {0x0B180C0D1C0E0F1E, 0X0101010101010101,                        \
+                     0x0B180C0D1C0E0F1E, 0X0101010101010101};                       \
+    __m256i shuf4 = {0x0001000100000000, 0x0003000300020002,                        \
+                     0x0005000500040004, 0x0007000700060006};                       \
+    YUV2RGB_LOAD_COE                                                                \
+                                                                                    \
+    h_size = c->dstW >> 4;                                                          \
+    vshift = c->srcFormat != AV_PIX_FMT_YUV422P;                                    \
+    for (y = 0; y < srcSliceH; y++) {                                               \
+        dst_type *image   = dst[0] + (y + srcSliceY) * dstStride[0];                \
+        const uint8_t *py = src[0] +               y * srcStride[0];                \
+        const uint8_t *pu = src[1] +   (y >> vshift) * srcStride[1];                \
+        const uint8_t *pv = src[2] +   (y >> vshift) * srcStride[2];                \
+        for (w = 0; w < h_size; w ++) {                                             \
+
+#define END_FUNC()                                                                  \
+        }                                                                           \
+    }                                                                               \
+    return srcSliceH;                                                               \
+}
+
+YUV2RGBFUNC(yuv420_rgb24, uint8_t, 0)
+    LOAD_YUV_16
+    YUV2RGB
+    RGB_PACK_16(r, g, b, rgb_l, rgb_h);
+    RGB_STORE(rgb_l, rgb_h, image, w);
+    END_FUNC()
+
+YUV2RGBFUNC(yuv420_bgr24, uint8_t, 0)
+    LOAD_YUV_16
+    YUV2RGB
+    RGB_PACK_16(b, g, r, rgb_l, rgb_h);
+    RGB_STORE(rgb_l, rgb_h, image, w);
+    END_FUNC()
+
+
+av_cold SwsFunc ff_yuv2rgb_init_loongarch(SwsContext *c)
+{
+    int cpu_flags = av_get_cpu_flags();
+#if HAVE_LASX
+    if (have_lasx(cpu_flags)) {
+        switch (c->dstFormat) {
+            case AV_PIX_FMT_RGB24:
+                return yuv420_rgb24;
+            case AV_PIX_FMT_BGR24:
+                return yuv420_bgr24;
+        }
+    }
+#endif  /* #if HAVE_LASX */
+    return NULL;
+}
+
